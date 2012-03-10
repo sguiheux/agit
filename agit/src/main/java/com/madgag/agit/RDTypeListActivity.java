759cd28844e8a13c0c4f80549ad19cd6708d0f70
Vv <vv@targate.fr>
2012-03-10 13:47:47 +0100

    Ajout menu contextuel

--- agit/src/main/java/com/madgag/agit/RDTypeListActivity.java
+++ agit/src/main/java/com/madgag/agit/RDTypeListActivity.java
@@ -19,10 +19,27 @@
 
 package com.madgag.agit;
 
+import static android.R.layout.simple_list_item_2;
+import static com.google.inject.name.Names.named;
+import static com.madgag.agit.GitIntents.OPEN_GIT_INTENT_PREFIX;
+import static com.madgag.agit.R.id.actionbar;
+import static com.madgag.agit.R.layout.list_activity_layout;
+import static com.madgag.agit.RepoScopedActivityBase.enterRepositoryScopeFor;
+import static com.madgag.android.listviews.ViewInflator.viewInflatorFor;
+
+import org.eclipse.jgit.lib.Repository;
+
+import roboguice.activity.RoboListActivity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+
 import com.google.inject.Inject;
 import com.google.inject.Key;
 import com.madgag.agit.git.model.RepoDomainType;
@@ -32,53 +49,80 @@ import com.madgag.android.listviews.ViewHolder;
 import com.madgag.android.listviews.ViewHolderFactory;
 import com.madgag.android.listviews.ViewHoldingListAdapter;
 import com.markupartist.android.widget.ActionBar;
-import org.eclipse.jgit.lib.Repository;
-import roboguice.activity.RoboListActivity;
-
-import static android.R.layout.simple_list_item_2;
-import static com.google.inject.name.Names.named;
-import static com.madgag.agit.GitIntents.OPEN_GIT_INTENT_PREFIX;
-import static com.madgag.agit.R.id.actionbar;
-import static com.madgag.agit.R.layout.list_activity_layout;
-import static com.madgag.agit.RepoScopedActivityBase.enterRepositoryScopeFor;
-import static com.madgag.android.listviews.ViewInflator.viewInflatorFor;
 
 public class RDTypeListActivity<E> extends RoboListActivity {
-	
+
 	public static Intent listIntent(Repository repository, String typeName) {
-		return new GitIntentBuilder(typeName+".LIST").repository(repository).toIntent();
+		return new GitIntentBuilder(typeName + ".LIST").repository(repository)
+				.toIntent();
 	}
-	
+
 	private static final String TAG = "RDTL";
-	private @Inject RepositoryContext rc;
-    private @Inject Repository repository;
+	private @Inject
+	RepositoryContext rc;
+	private @Inject
+	Repository repository;
 	private RepoDomainType<E> rdt;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-        RepositoryScope repositoryScope = enterRepositoryScopeFor(this,getIntent());
+
+		RepositoryScope repositoryScope = enterRepositoryScopeFor(this,
+				getIntent());
 		try {
-            super.onCreate(savedInstanceState);
-            rdt = extractRDTFromIntent();
-        } finally {
-            repositoryScope.exit();
-        }
+			super.onCreate(savedInstanceState);
+			rdt = extractRDTFromIntent();
+		} finally {
+			repositoryScope.exit();
+		}
 
 		setContentView(list_activity_layout);
 		ActionBar actionBar = (ActionBar) findViewById(actionbar);
-        actionBar.setHomeAction(new HomeAction(this));
+		actionBar.setHomeAction(new HomeAction(this));
 		actionBar.setTitle(rdt.conciseSummaryTitle());
-		setListAdapter(new ViewHoldingListAdapter<E>(rdt.getAll(), getViewFactory()));
+
+		setListAdapter(new ViewHoldingListAdapter<E>(rdt.getAll(),
+				getViewFactory()));
+
+		registerForContextMenu(getListView());
+
 	}
-	
+
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
-		startActivity(rdt.viewIntentFor((E)getListAdapter().getItem(position)));
+		startActivity(rdt.viewIntentFor((E) getListAdapter().getItem(position)));
 	}
-	
+
+	/**
+	 * Add Context Menu to use git command
+	 */
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.gitoperation, menu);
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onContextItemSelected(android.view.MenuItem)
+	 */
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		Dialog dia = new Dialog(this);
+		dia.setTitle(item.getTitle());
+		dia.show();
+		return super.onContextItemSelected(item);
+	}
+
 	private RepoDomainType<E> extractRDTFromIntent() {
-		String rdtName = getIntent().getAction().substring(OPEN_GIT_INTENT_PREFIX.length()).split("\\.")[0];
-        return getInjector().getInstance(Key.get(RepoDomainType.class, named(rdtName)));
+		String rdtName = getIntent().getAction()
+				.substring(OPEN_GIT_INTENT_PREFIX.length()).split("\\.")[0];
+		return getInjector().getInstance(
+				Key.get(RepoDomainType.class, named(rdtName)));
 	}
 
 	@Override
@@ -99,12 +143,12 @@ public class RDTypeListActivity<E> extends RoboListActivity {
 		rc.onDestroy();
 	}
 
-
-    public ViewFactory<E> getViewFactory() {
-        return  new ViewFactory<E>(viewInflatorFor(this, simple_list_item_2), new ViewHolderFactory<E>() {
-            public ViewHolder<E> createViewHolderFor(View view) {
-                return new RDTypeInstanceViewHolder(rdt, view);
-            }
-        });
-    }
+	public ViewFactory<E> getViewFactory() {
+		return new ViewFactory<E>(viewInflatorFor(this, simple_list_item_2),
+				new ViewHolderFactory<E>() {
+					public ViewHolder<E> createViewHolderFor(View view) {
+						return new RDTypeInstanceViewHolder(rdt, view);
+					}
+				});
+	}
 }
--- agit/src/main/java/com/madgag/agit/RepositoryViewerActivity.java
+++ agit/src/main/java/com/madgag/agit/RepositoryViewerActivity.java
@@ -19,17 +19,38 @@
 
 package com.madgag.agit;
 
+import static android.R.drawable.ic_menu_delete;
+import static android.content.DialogInterface.BUTTON_POSITIVE;
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+import static com.madgag.agit.GitIntents.REPO_STATE_CHANGED_BROADCAST;
+import static com.madgag.agit.GitIntents.actionWithSuffix;
+import static com.madgag.agit.GitIntents.gitDirFrom;
+import static com.madgag.agit.R.drawable.ic_title_fetch;
+import static com.madgag.agit.R.string.button_no;
+import static com.madgag.agit.R.string.button_yes;
+import static com.madgag.agit.R.string.repo_deletion_dialog_confirmation_message;
+import static com.madgag.agit.R.string.repo_deletion_dialog_confirmation_title;
+import static com.madgag.agit.git.Repos.niceNameFor;
+
+import java.io.File;
+
+import roboguice.inject.InjectView;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.PendingIntent;
 import android.app.ProgressDialog;
-import android.content.*;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+
 import com.google.inject.Inject;
 import com.madgag.agit.operation.lifecycle.CasualShortTermLifetime;
 import com.madgag.agit.operations.GitAsyncTaskFactory;
@@ -37,106 +58,107 @@ import com.madgag.agit.operations.GitOperationExecutor;
 import com.madgag.agit.operations.RepoDeleter;
 import com.markupartist.android.widget.ActionBar;
 import com.markupartist.android.widget.ActionBar.Action;
-import roboguice.inject.InjectView;
-
-import java.io.File;
-
-import static android.R.drawable.ic_menu_delete;
-import static android.content.DialogInterface.BUTTON_POSITIVE;
-import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
-import static com.madgag.agit.GitIntents.*;
-import static com.madgag.agit.R.drawable.ic_title_fetch;
-import static com.madgag.agit.R.string.*;
-import static com.madgag.agit.git.Repos.niceNameFor;
-
 
 public class RepositoryViewerActivity extends RepoScopedActivityBase {
 
-    public static final String TAG = "RMA";
+	public static final int READ_MODE = 0;
+	public static final int WRITE_MODE = 1;
+	private int readWriteFlag = READ_MODE;
+
+	public static final String TAG = "RMA";
 
 	public static Intent manageRepoIntent(File gitdir) {
 		return new GitIntentBuilder("repo.VIEW").gitdir(gitdir).toIntent();
 	}
 
-	private final static int DELETE_ID=Menu.FIRST;
-    private final int DELETION_IN_PROGRESS_DIALOG =3;
-	private final int DELETION_CONFIRMATION_DIALOG =DELETION_IN_PROGRESS_DIALOG + 1;
+	private final static int DELETE_ID = Menu.FIRST;
+	private final int DELETION_IN_PROGRESS_DIALOG = 3;
+	private final int DELETION_CONFIRMATION_DIALOG = DELETION_IN_PROGRESS_DIALOG + 1;
 
 	private DialogInterface.OnClickListener DELETION_CONFIRMATION_DIALOG_LISTENER = new DialogInterface.OnClickListener() {
 		public void onClick(DialogInterface dialogInterface, int which) {
-			if (which== BUTTON_POSITIVE) {
+			if (which == BUTTON_POSITIVE) {
 				doDeletion();
 			}
 		}
 	};
 
-    @Inject GitAsyncTaskFactory gitAsyncTaskFactory;
+	@Inject
+	GitAsyncTaskFactory gitAsyncTaskFactory;
 
-    @InjectView(R.id.actionbar) ActionBar actionBar;
-    @InjectView(android.R.id.list) ListView listView;
+	@InjectView(R.id.actionbar)
+	ActionBar actionBar;
+	@InjectView(android.R.id.list)
+	ListView listView;
 
-    @Inject RepoSummaryAdapter summaryAdapter;
-    @Inject GitOperationExecutor gitOperationExecutor;
+	@Inject
+	RepoSummaryAdapter summaryAdapter;
+	@Inject
+	GitOperationExecutor gitOperationExecutor;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.repo_management_activity);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.repo_management_activity);
 
-        actionBar.setHomeAction(new HomeAction(this));
+		actionBar.setHomeAction(new HomeAction(this));
 		actionBar.setTitle(niceNameFor(repo()));
-        actionBar.addAction(new Action() {
-            public void performAction(View view) {
-                startService(new GitIntentBuilder("repo.SYNC").repository(repo()).toIntent());
-            }
-			
+		actionBar.addAction(new Action() {
+			public void performAction(View view) {
+				startService(new GitIntentBuilder("repo.SYNC").repository(
+						repo()).toIntent());
+			}
+
 			public int getDrawable() {
 				return ic_title_fetch;
 			}
-        });
+		});
 
-        listView.setOnItemClickListener(summaryAdapter.getOnItemClickListener());
-    }
+		listView.setOnItemClickListener(summaryAdapter.getOnItemClickListener());
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-    	super.onCreateOptionsMenu(menu);
-        menu.add(0, DELETE_ID, 0, R.string.delete_repo_menu_option).setShortcut('0', 'd').setIcon(ic_menu_delete);
-        return true;
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-        case DELETE_ID:
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		menu.add(0, DELETE_ID, 0, R.string.delete_repo_menu_option)
+				.setShortcut('0', 'd').setIcon(ic_menu_delete);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case DELETE_ID:
 			showDialog(DELETION_CONFIRMATION_DIALOG);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
 	private void doDeletion() {
 		showDialog(DELETION_IN_PROGRESS_DIALOG);
 
-		gitAsyncTaskFactory.createTaskFor(new RepoDeleter(repo()), new CasualShortTermLifetime()).execute();
+		gitAsyncTaskFactory.createTaskFor(new RepoDeleter(repo()),
+				new CasualShortTermLifetime()).execute();
 	}
 
 	BroadcastReceiver operationProgressBroadcastReceiver = new BroadcastReceiver() {
 		public void onReceive(Context context, Intent intent) {
 			String action = intent.getAction();
-			Log.d(TAG, "Got broadcast : "+action);
+			Log.d(TAG, "Got broadcast : " + action);
 		}
 	};
-	
+
 	BroadcastReceiver repoStateChangeBroadcastReceiver = new BroadcastReceiver() {
 		public void onReceive(Context context, Intent intent) {
-			Log.d(TAG, "repoStateChangeBroadcastReceiver got broadcast : "+intent);
+			Log.d(TAG, "repoStateChangeBroadcastReceiver got broadcast : "
+					+ intent);
 			if (!gitdir().exists()) {
 				finish();
 			}
 		}
 	};
-	
+
 	protected Dialog onCreateDialog(int id) {
 		switch (id) {
 		case DELETION_IN_PROGRESS_DIALOG:
@@ -150,57 +172,60 @@ public class RepositoryViewerActivity extends RepoScopedActivityBase {
 					.setIcon(R.drawable.icon)
 					.setTitle(repo_deletion_dialog_confirmation_title)
 					.setMessage(repo_deletion_dialog_confirmation_message)
-					.setPositiveButton(button_yes, DELETION_CONFIRMATION_DIALOG_LISTENER)
-					.setNegativeButton(button_no, DELETION_CONFIRMATION_DIALOG_LISTENER)
-					.create();
+					.setPositiveButton(button_yes,
+							DELETION_CONFIRMATION_DIALOG_LISTENER)
+					.setNegativeButton(button_no,
+							DELETION_CONFIRMATION_DIALOG_LISTENER).create();
 		default:
 			return super.onCreateDialog(id);
 		}
 	}
-	
+
 	@Override
 	protected void onNewIntent(Intent newIntent) {
-		Log.i(TAG, "onNewIntent called with "+newIntent+" "+gitDirFrom(newIntent));
+		Log.i(TAG, "onNewIntent called with " + newIntent + " "
+				+ gitDirFrom(newIntent));
 	}
-	
-    @Override
-    protected void onResume() {
-    	super.onResume();
-		registerReceiver(operationProgressBroadcastReceiver, new IntentFilter("org.openintents.git.operation.progress.update"));
-
-		registerReceiver(repoStateChangeBroadcastReceiver, new IntentFilter(actionWithSuffix(REPO_STATE_CHANGED_BROADCAST)));
-		
-		updateUI();
-		
-    }
-
-	void updateUI() {
-        listView.setAdapter(summaryAdapter);
-//        TextView remotesSummary = (TextView) findViewById(remotes_summary);
-//        remotesSummary.setText(new RDTRemote(repo()).summariseAll());
-//
-//        TextView branchesSummary = (TextView) findViewById(branches_summary);
-//        branchesSummary.setText(new RDTBranch(repo()).summariseAll());
-//
-//        TextView tagsSummary = (TextView) findViewById(tags_summary);
-//        tagsSummary.setText(new RDTTag(repo()).summariseAll());
-	}
-
-
 
 	@Override
-    protected void onPause() {
-    	super.onPause();
-    	unregisterReceiver(operationProgressBroadcastReceiver);
-        unregisterReceiver(repoStateChangeBroadcastReceiver);
-    }
-	
-	public static PendingIntent manageRepoPendingIntent(File gitdir,Context context) {
-		Log.i(TAG, "manageRepoPendingIntent yeah - creating with "+gitdir);
-		Intent intentForNotification = manageRepoIntent(gitdir);
-        intentForNotification.setFlags(FLAG_ACTIVITY_NEW_TASK);
-		return PendingIntent.getActivity(context, gitdir.hashCode(), intentForNotification, 0);
+	protected void onResume() {
+		super.onResume();
+		registerReceiver(operationProgressBroadcastReceiver, new IntentFilter(
+				"org.openintents.git.operation.progress.update"));
+
+		registerReceiver(repoStateChangeBroadcastReceiver, new IntentFilter(
+				actionWithSuffix(REPO_STATE_CHANGED_BROADCAST)));
+
+		updateUI();
+
 	}
 
+	void updateUI() {
+		listView.setAdapter(summaryAdapter);
+		// TextView remotesSummary = (TextView) findViewById(remotes_summary);
+		// remotesSummary.setText(new RDTRemote(repo()).summariseAll());
+		//
+		// TextView branchesSummary = (TextView) findViewById(branches_summary);
+		// branchesSummary.setText(new RDTBranch(repo()).summariseAll());
+		//
+		// TextView tagsSummary = (TextView) findViewById(tags_summary);
+		// tagsSummary.setText(new RDTTag(repo()).summariseAll());
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		unregisterReceiver(operationProgressBroadcastReceiver);
+		unregisterReceiver(repoStateChangeBroadcastReceiver);
+	}
+
+	public static PendingIntent manageRepoPendingIntent(File gitdir,
+			Context context) {
+		Log.i(TAG, "manageRepoPendingIntent yeah - creating with " + gitdir);
+		Intent intentForNotification = manageRepoIntent(gitdir);
+		intentForNotification.setFlags(FLAG_ACTIVITY_NEW_TASK);
+		return PendingIntent.getActivity(context, gitdir.hashCode(),
+				intentForNotification, 0);
+	}
 
 }
